cca0b18090cd94b49e91520488459215a7f27b79
Ali Khaled <50497959+ali-khaled-949@users.noreply.github.com>
2022-09-13 05:28:35 -0700

    Update home.cpp

--- home.cpp
+++ home.cpp
@@ -1,6 +1,6 @@
 void display ()
 {
-   ifstream in ("scoreboard.txt");
+   ifstream in ("sample_input.txt");
    string current="";//always a good Idea to intialize your variables
    
    if (!in){cout <<" text file not found"; exit (1);}
